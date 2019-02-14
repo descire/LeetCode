de73eb0f737016aca7044222e9af842caa00ab53
descire <daiqingyunc@gmail.com>
2019-02-14 11:16:13 +0800

    feat(dp): :sparkles:动态规划解决674

---
+++ dynamic-programming/674/solution1.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ * 
+ * 674. Longest Continuous Increasing Subsequence
+ * 
+ * [Easy]
+ * 
+ * dp
+ * 
+ * 定义状态
+ * 
+ *  dp[i]表示以下标i结尾的递增序列的长度
+ * 
+ * 边界状态
+ * 
+ *  dp[0] = 1
+ * 
+ * 状态转移方程
+ *             1               nums[i] <= nums[i - 1]
+ *  dp[i] =
+ *             dp[i - 1] + 1   nums[i] > nums[i - 1]
+ * 
+ * 
+ */
+
+const findLengthOfLCIS = nums => {
+  const max = nums.length
+  let ans = 0
+  if (max === 0) {
+    return ans
+  }
+  ans = 1
+  if (max === 1) {
+    return ans
+  }
+
+  let current = 1 // 当前递增序列的长度
+  let preItem = nums[0] // 前一个元素
+
+  for (let i = 1; i < max; i++) {
+    const item = nums[i]
+    if (item > preItem) {
+      current++
+    } else {
+      current = 1
+    }
+    preItem = item
+    ans = Math.max(ans, current)
+  }
+  return ans
+}
