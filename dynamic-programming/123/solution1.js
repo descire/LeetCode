a10e82fb65270e29d1dac49a14b130d757fc522a
descire <daiqingyunc@gmail.com>
2019-02-18 11:22:15 +0800

    feat(dp): :sparkles:添加121新解法

---
+++ dynamic-programming/123/solution1.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+ * 
+ * 123. Best Time to Buy and Sell Stock III
+ * 
+ * Hard
+ */
+const maxProfit = prices => {
+  const max = prices.length
+  if (max < 2) {
+    return 0
+  }
+  
+}
