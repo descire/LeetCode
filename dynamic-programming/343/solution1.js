c835a06bab601045786d71e597ac38d4860c68aa
descire <daiqingyunc@gmail.com>
2019-02-20 21:38:03 +0800

    feat(dp): :sparkles:【动态规划】解决343

---
+++ dynamic-programming/343/solution1.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/integer-break/
+ * 
+ * 343. Integer Break
+ * 
+ * Medium
+ * 
+ */
+const integerBreak = n => {
+  const dp = new Array(n + 1).fill(1)
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i + 1; j++) {
+      if (i + j <= n) {
+        const x = Math.max(dp[i], i)
+        const y = Math.max(dp[j], j)
+
+        dp[i + j] = Math.max(x * y, dp[i + j])
+      }
+    }
+  }
+
+  return dp[n]
+}
