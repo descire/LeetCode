df3c5b8027c13243618b90b1f1f99607aadaf154
descire <daiqingyunc@gmail.com>
2019-02-21 21:48:12 +0800

    feat(dp): :sparkles:[遍历]解决392

---
+++ dynamic-programming/392/solution1.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ * https://leetcode.com/problems/is-subsequence/
+ * 
+ * 392. Is Subsequence
+ * 
+ * Medium
+ */
+const isSubsequence = (s, t) => {
+  const l1 = s.length - 1
+  const l2 = t.length - 1
+
+  let s1 = 0
+  let e1 = l1
+
+  let s2 = 0
+  let e2 = l2
+
+  while (s1 <= e1 && s2 <= e2) {
+    const pre1 = s[s1]
+    const next1 = s[e1]
+
+    const pre2 = t[s2]
+    const next2 = t[e2]
+
+    if (pre2 === pre1) {
+      s2++
+      s1++
+    } else {
+      s2++
+    }
+    if (next2 === next1) {
+      e1--
+      e2--
+    } else {
+      e2--
+    }
+  }
+
+  if (s1 > e1) {
+    return true
+  } else {
+    return false
+  }
+}
