6836e0006cc116ec7cf4dfe845b641de8b232a95
descire <daiqingyunc@gmail.com>
2019-02-26 15:45:40 +0800

    feat(dp): :sparkles:动态规划的方法处理494

--- dynamic-programming/494/solution2.js
+++ dynamic-programming/494/solution2.js
@@ -6,5 +6,24 @@
  * Medium
  */
 const findTargetSumWays = (nums, S) => {
-  
+  const max = nums.length
+
+  const sum = nums.reduce((x, y) => x + y)
+
+  // 特殊情况
+  if (S > sum) {
+    return 0
+  }
+
+  const dp = []
+  dp[0] = new Array(sum * 2 + 1).fill(0) // 纯属为了打印出来好看^_^
+  dp[0][sum] = 1
+
+  for (let i = 1; i <= max; i++) {
+    dp[i] = []
+    for (let j = -sum; j <= sum; j++) {
+      dp[i][j + sum] = (dp[i - 1][j - nums[i - 1] + sum] || 0) + (dp[i - 1][j + nums[i - 1] + sum] || 0)
+    }
+  }
+  return dp[max][S + sum]
 }
