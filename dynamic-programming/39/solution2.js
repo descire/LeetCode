7600a5d8f4be7bae08970b8269feebc4b6917d6d
valar_cC <15751165579@163.com>
2019-03-02 23:37:47 +0800

    feat(dp): :sparkles:优化39解法

---
+++ dynamic-programming/39/solution2.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * https://leetcode.com/problems/combination-sum/
+ * 
+ * 39. Combination Sum
+ * 
+ * Medium
+ * 
+ * DFS + hashmap
+ * 
+ */
+var combinationSum = function(candidates, target) {
+  const ans = []
+  const dfs = (currentIndex, currentArray, target) => {
+    if (target === 0) {
+      ans.push(currentArray.slice())
+      return 
+    }
+    if (target < 0) {
+      return
+    }
+
+    for (let i = currentIndex; i < candidates.length; ++i) {
+      currentArray.push(candidates[i])
+      dfs(i, currentArray, target - candidates[i])
+      currentArray.pop()
+    }
+  }
+  dfs(0, [], target)
+  return ans
+}
