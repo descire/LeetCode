8003106df5c1b8fead80285cd38da8290aee52b9
descire <daiqingyunc@gmail.com>
2019-03-07 20:38:35 +0800

    feat(dp): :sparkles:[动态规划]解决279

---
+++ dynamic-programming/279/solution1.js
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/perfect-squares/
+ * 
+ * 279. Perfect Squares
+ * 
+ * Medium
+ */
+const numSquares = n => {
+  const dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER)
+  dp[1] = 1
+  for (let i = 2; i <= n; i++) {
+    const s = Math.floor(Math.sqrt(i))
+    for (let j = i - 1; j >= 1; j--) {
+      if (Math.pow(s, 2) === i) {
+        dp[i] = 1
+      }
+      dp[i] = Math.min(dp[j] + dp[i - j], dp[i])
+    }
+  }
+  return dp[n]
+}
