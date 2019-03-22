99679e31c43db192ae73377ea677e74d4acb7b82
descire <daiqingyunc@gmail.com>
2019-03-22 17:30:18 +0800

    docs(readme): :memo:更新目录导航

--- dynamic-programming/983/solution1.js
+++ dynamic-programming/983/solution1.js
@@ -16,7 +16,7 @@ const mincostTickets = (days, costs) => {
 
   const dp = [0]
   for (let i = 1; i <= max; i++) {
-    dp[i] = costs[0] + dp[i - 1]
+    dp[i] = Math.min(costs[0] + dp[i - 1], costs[1] + dp[i - 1], costs[2] + dp[i - 1])
     let dis = 0
     for (let j = i - 1; j >= 1; j--) {
       dis = days[i - 1] - days[j - 1]
