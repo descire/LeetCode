1a3f504df6aef87b92c064e63b8edeb453f4b76a
valar_cC <15751165579@163.com>
2019-03-26 21:24:17 +0800

    feat(dp): :sparkles:[动态规划]解决322

--- dynamic-programming/322/solution2.js
+++ dynamic-programming/322/solution2.js
@@ -14,7 +14,18 @@ const coinChange = (coins, amount) => {
   if (!max) {
     return -1
   }
-  if (amount === 0) {
-    return 0
+  const dp = new Array(max + 1)
+  dp[0] = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER)
+  dp[0][0] = 0
+
+  for (let i = 1; i <= max; i++) {
+    dp[i] = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER)
+    dp[i][0] = 0
+    const coin = coins[i - 1]
+    for (let j = 1; j <= amount; j++) {
+      dp[i][j] = Math.min(dp[i][j], dp[i][j - coin] + 1 || Number.MAX_SAFE_INTEGER, dp[i - 1][j])
+    }
   }
+
+  return (dp[max][amount] === Number.MAX_SAFE_INTEGER ? -1 : dp[max][amount])
 }
