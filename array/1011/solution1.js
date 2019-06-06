7fba788076c38cd4a61da65bd340d6f19cb613fd
valar_cC <15751165579@163.com>
2019-06-06 18:29:02 +0800

    feat(array): :sparkles:二分查找解决1011

---
+++ array/1011/solution1.js
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ * 
+ * 1011. Capacity To Ship Packages Within D Days
+ * 
+ * Medium
+ */
+const shipWithinDays = (weights, D) => {
+  const max = weights.length
+}
