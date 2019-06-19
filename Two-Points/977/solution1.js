65a94f39d153b8e39acf7b0a8d04b633635a9cc9
valar_cC <15751165579@163.com>
2019-06-19 23:31:10 +0800

    feat(two-points): :sparkles:双指针解决977

---
+++ Two-Points/977/solution1.js
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/squares-of-a-sorted-array/
+ * 
+ * 977. Squares of a Sorted Array
+ * 
+ * Easy
+ * 
+ * 一般的思路
+ * 
+ * 先求出所有的数的 平方 O(n)
+ * 
+ * 再排序 O(nlogn)
+ * 
+ * 题目本身就是一个非递减的数组
+ * 
+ * 负数 非负数 
+ * O(n)
+ */
+const sortedSquares = A => {
+  const result = []
+  let max = A.length
+  let start = 0
+  let end = max - 1
+  while (start <= end) {
+    const x = A[start] ** 2
+    const y = A[end] ** 2
+    if (x <= y) {
+      result.push(y)
+      end--
+    } else {
+      result.push(x)
+      start++
+    }
+  }
+  return result.reverse()
+}
