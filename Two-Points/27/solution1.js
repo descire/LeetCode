968e0bf72e8fa1c8134b87ff152b92e8767c55e7
descire <daiqingyunc@gmail.com>
2019-06-21 12:13:52 +0800

    feat(two-points): :sparkles:双指针解决27

---
+++ Two-Points/27/solution1.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode-cn.com/problems/remove-element/
+ * 
+ * 27. 移除元素
+ * 
+ * Easy
+ * 
+ * 97.20%
+ * 33.05%
+ * 
+ * 注意终止条件的判断 start === end 时
+ */
+const removeElement = (nums, val) => {
+  const max = nums.length
+  let start = 0
+  let end = max - 1
+
+  while (start < end) {
+    if (nums[end] === val) {
+      nums.pop()
+      end--
+      continue
+    }
+    if (nums[start] === val) {
+      nums[start] = nums[end]
+      nums.pop()
+      end--
+      start++
+      continue
+    }
+    start++
+  }
+  if (nums[end] === val) {
+    nums.pop()
+  }
+  return nums.length
+}
