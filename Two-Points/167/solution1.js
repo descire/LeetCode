730214ea730cf16c5d03f3b1d7378b9bb08e8f47
descire <daiqingyunc@gmail.com>
2019-06-21 12:27:29 +0800

    feat(two-points): :sparkles:双指针解决167

---
+++ Two-Points/167/solution1.js
@@ -0,0 +1,31 @@
+  /**
+   * https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+   * 
+   * 167. 两数之和 II - 输入有序数组
+   * 
+   * Easy
+   * 
+   * 91.86%
+   * 57.10%
+   */
+  const twoSum = (numbers, target) => {
+    const max = numbers.length
+    let start = 0
+    let end = max - 1
+
+    while (start < end) {
+      const rest = target - numbers[end]
+      if (rest < numbers[start]) {
+        end--
+        continue
+      }
+      if (rest > numbers[start]) {
+        start++
+        continue
+      }
+      if (rest === numbers[start]) {
+        return [start + 1, end  + 1]
+      }
+    }
+    return []
+  }
