34dbe616edd9bc2fdcce10a395d36d4961d9eb7b
valar_cC <15751165579@163.com>
2019-06-21 11:52:58 +0800

    feat(two-points): :sparkles:优化344解法

--- Two-Points/344/solution1.js
+++ Two-Points/344/solution1.js
@@ -4,19 +4,15 @@
  * 344. Reverse String
  * 
  * Easy
- * 
- * 104ms 98.36%
- * 46.7mb 79.72%
+ * 93.72%
+ * 26.52%
  */
 const reverseString = s => {
   let start = 0
   let end = s.length - 1
   while (start < end) {
-    const item = s[start]
-    s[start] = s[end]
-    s[end] = item
+    [s[start], s[end]] = [s[end], s[start]]
     start++
     end--
   }
-  return s
 }
