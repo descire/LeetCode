7d558d769ddadb9931636a85721b800ac91a02e7
valar_cC <15751165579@163.com>
2019-06-21 12:01:42 +0800

    feat(two-points): :sparkles:优化349

--- Two-Points/349/solution1.js
+++ Two-Points/349/solution1.js
@@ -5,20 +5,19 @@
  * 
  * Easy
  * 
- * 48ms 99.34%
- * 34.8mb 57.93%
+ * 97.18%
+ * 13.61%
  * 
  * 非双指针题目 
  * 采用 集合处理
  */
 const intersection = (nums1, nums2) => {
   const s1 = new Set(nums1)
-  const s2 = new Set(nums2)
-  const result = []
-  for (let num of s1.values()) {
-    if (s2.has(num)) {
-      result.push(num)
+  const s2 = new Set()
+  for (let num of nums2) {
+    if (s1.has(num)) {
+      s2.add(num)
     }
   }
-  return result
+  return [...s2]
 }
