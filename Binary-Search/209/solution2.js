f66e3f0f55dcdaabd1875a03f3414c9741ff38d2
descire <daiqingyunc@gmail.com>
2019-07-01 15:30:35 +0800

    feat(binary-search): :sparkles:二分搜索算法解决209

--- Binary-Search/209/solution2.js
+++ Binary-Search/209/solution2.js
@@ -7,33 +7,45 @@
  * 
  * 52ms 96.09%
  * 35.9mb 17.26%
+ * 
+ * 第一眼看过去 这哪是一道二分搜索的题目
+ * 
+ * 两重循环直接搞定 O(n^2)
+ * 
+ * 第一种解法 
+ * 首先构建一个有序数组 前缀和数组
+ * 
+ * 那么只要在该有序数组中找出 sums[j] - sums[i] >= s 
+ * 
+ * 记录每一组的长度，最后给出最小长度即可。
  */
 const minSubArrayLen = (s, nums) => {
-  let ans = Number.MAX_SAFE_INTEGER
   const sums = [0]
   nums.forEach((item, index) => sums[index + 1] = sums[index] + item)
-
-  for (let i = 0, max = nums.length; i < max; i++) {
-    const num = nums[i]
-    const index = binarySearch(sums, 0, sums.length - 1, sums[i], s)
-    if (sums[index] - sums[i] >= s) {
+  let ans = Number.MAX_SAFE_INTEGER
+  for (let i = 0, max = sums.length; i < max; i++) {
+    const sum = sums[i]
+    // 二分搜索满足的和值
+    const index = binarySearch(sums, i, max, s, sum)
+    if (sums[index] - sum >= s) {
       ans = Math.min(ans, index - i)
     }
   }
+
   if (ans === Number.MAX_SAFE_INTEGER) {
     return 0
   }
   return ans
 }
 
-function binarySearch(array, first, last, target, s) {
-  while (first < last) {
-    const mid = Math.floor(first + (last - first) / 2)
-    if (array[mid] - target >= s) {
-      last = mid
+function binarySearch(array, start, end, target, base) {
+  while (start < end) {
+    const mid = Math.floor(start + (end - start) / 2)
+    if (array[mid] - base < target) {
+      start = mid + 1
     } else {
-      first = mid + 1
+      end = mid
     }
   }
-  return first
+  return start
 }
