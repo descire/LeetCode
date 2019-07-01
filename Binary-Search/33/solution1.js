43b91183308d136d6c3abd87cc3e760912f4606c
wuwangkeji <3253105365@qq.com>
2019-07-01 14:15:04 +0800

    feat(binary-search): :sparkles:更新33题解题代码

--- Binary-Search/33/solution1.js
+++ Binary-Search/33/solution1.js
@@ -7,37 +7,63 @@
  * 
  * 48ms 98.15%
  * 33.7mb 90.53%
+ * 
+ * 递增 递增 序列
+ * 
+ * 852. 山脉数组的峰顶索引
+ * 
+ * 162. 寻找峰值
+ * 
+ * 这两道题都是 递增 递减 
+ * 
+ * 从题目中可以得出两个信息 
+ * 1、递增 递增
+ * 2、对于任何一个数 知道它 落在 那个 递增区间里
+ * 
+ * 分为两部分处理
+ * 1、当前搜索区间只在一个递增区间上，那么和一般的二分搜索没啥区别
+ * 
+ * 2、当前搜索区间横跨两个递增区间上，这个就需要特殊处理
  */
 const search = (nums, target) => {
   const max = nums.length - 1
-  if (max === 0 && target === nums[0]) {
-    return 0
-  }
-  let first = 0
-  let last = max
-  while (first < last) {
-    const mid = Math.floor(first + (last - first) / 2)
-    if (nums[mid] === target) {
-      return mid
-    }
-    if (nums[mid] < nums[last]) {
-      // mid --- last 递增区间
-      if (nums[mid] < target && target <= nums[last]) {
-        first = mid + 1
+  let start = 0
+  let end = max
+
+  while (start < end) {
+    const mid = Math.floor(start + (end - start) / 2)
+    // 划分区间
+    if (nums[start] > nums[end]) {
+      // 当前搜索区间横跨两个递增数组
+
+      if (nums[mid] <= nums[end]) {
+        // 在第二个递增区间上
+        if (target > nums[mid] && target <= nums[end]) {
+          start = mid + 1
+        } else {
+          end = mid
+        }
       } else {
-        last = mid - 1
+        // 在第一个递增区间
+        if (target <= nums[mid] && target > nums[end]) {
+          end = mid
+        } else {
+          start = mid + 1
+        }
       }
-    } else if (nums[mid] > nums[last]) {
-      // first ---- mid 递增区间
-      if (nums[first] <= target && target < nums[mid]) {
-        last = mid - 1
+
+    } else {
+      // 只能在一个递增数组上搜索，属于正常情况
+      if (nums[mid] < target) {
+        start = mid + 1
       } else {
-        first = mid + 1
+        end = mid
       }
     }
   }
-  if (nums[first] === target) {
-    return first
+
+  if (nums[start] === target) {
+    return start
   }
   return -1
 }
