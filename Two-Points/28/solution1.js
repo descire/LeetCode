660fcfcc08b1ab443481ae6f4cdf50d4fbfe0324
descire <daiqingyunc@gmail.com>
2019-07-05 14:51:07 +0800

    feat(two-points): :sparkles:优化字符串匹配算法

--- Two-Points/28/solution1.js
+++ Two-Points/28/solution1.js
@@ -7,13 +7,17 @@
  * 
  * 暴力求解
  * 
+ * 88ms 68.90%
+ * 
+ * 35.3mb 17.83%
+ * 
  * O(m*n)
  */
 const strStr = (haystack, needle) => {
   if (needle === '') {
     return 0
   }
-
+  
   for (let i = 0, max = haystack.length; i < max; i++) {
     if (isValid(haystack, needle, i)) {
       return i
@@ -22,13 +26,19 @@ const strStr = (haystack, needle) => {
   return -1
 }
 
-function isValid(str1, str2, start) {
-
-  for (let i = start, max = start + str2.length; i < max; i++) {
-    if (str1[i] !== str2[i - start]) {
+function isValid(haystack, needle, index) {
+  const max = needle.length - 1
+  let start = index
+  let end = start + max
+  while (start <= end) {
+    if (needle[start - index] !== haystack[start]) {
       return false
     }
+    if (needle[end - index] !== haystack[end]) {
+      return false
+    }
+    start++
+    end--
   }
-
   return true
 }
