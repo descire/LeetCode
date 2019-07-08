4e4a13882c8891c54fe9ea0ce31f4e7a38c0e971
valar_cC <15751165579@163.com>
2019-07-08 12:08:27 +0800

    feat(two-points): :sparkles:双指针解决881

---
+++ Two-Points/881/solution1.js
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode-cn.com/problems/boats-to-save-people/
+ * 
+ * 881. 救生艇
+ * 
+ * Medium
+ * 
+ * 标准的双指针
+ * 
+ * 228ms 85.00%
+ * 42.9mb 20.0%
+ */
+const numRescueBoats = (people, limit) => {
+  people.sort((a, b) => a - b)
+
+  let start = 0
+  let end = people.length - 1
+  let ans = 0
+  while (start <= end) {
+    const smaller = people[start]
+    const bigger = people[end]
+    if (smaller + bigger > limit) {
+      end--
+    } else {
+      end--
+      start++
+    }
+    ans++
+  }
+  return ans
+}
+
+console.log(numRescueBoats([1, 2], 3)) // 1
+console.log(numRescueBoats([3, 2, 2, 1], 3)) // 3
+console.log(numRescueBoats([3, 5, 3, 4], 5)) // 4
