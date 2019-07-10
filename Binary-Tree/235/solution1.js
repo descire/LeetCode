8bd47b2d8366fa2853f697d96d2ef87afa633391
descire <daiqingyunc@gmail.com>
2019-07-11 00:31:13 +0800

    feat(binary-tree): :sparkles:递归处理235

--- Binary-Tree/235/solution1.js
+++ Binary-Tree/235/solution1.js
@@ -1,3 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * 
+ * 235. 二叉搜索树的最近公共祖先
+ * 
+ * Easy
+ * 
+ * 
+ * 112ms 92.88%
+ * 44.3mb 10.19%
+ */
 const lowestCommonAncestor = (root, p, q) => {
   if (!root) {
     return null
