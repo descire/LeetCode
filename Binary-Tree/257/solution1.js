55b9af7d91c5d03d24236a2e522c8c5b82d546cf
descire <daiqingyunc@gmail.com>
2019-07-11 00:49:08 +0800

    feat(binary-tree): :sparkles:递归处理257

--- Binary-Tree/257/solution1.js
+++ Binary-Tree/257/solution1.js
@@ -1,21 +1,33 @@
+/**
+ * 257. 二叉树的所有路径
+ * 
+ * https://leetcode-cn.com/problems/binary-tree-paths/
+ * 
+ * Easy
+ * 
+ * 72ms 98.09%
+ * 35mb 6.00%
+ * 
+ */
 const binaryTreePaths = root => {
   const ans = []
   if (!root) {
     return ans
   }
 
-  preOrder(root, String(root.val))
+  order(root, String(root.val), ans)
   return ans
-  function preOrder (root, path) {
-    if (!root) {
-      return
-    }
-    if (!root.left && !root.right) {
-      ans.push(path)
-      return
-    }
+}
 
-    root.left && preOrder(root.left, path + '->' + root.left.val)
-    root.right && preOrder(root.right, path + '->' + root.right.val)
+function order (root, path, ans) {
+  if (!root) {
+    return
   }
+  if (!root.left && !root.right) {
+    ans.push(path)
+    return
+  }
+
+  root.left && order(root.left, path + '->' + root.left.val, ans)
+  root.right && order(root.right, path + '->' + root.right.val, ans)
 }
