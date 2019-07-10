9d3f7a022546e65e99468f6aa438c40d6c076846
descire <daiqingyunc@gmail.com>
2019-07-10 23:09:37 +0800

    feat(binary-tree): :sparkles:递归解决104

--- Binary-Tree/104/solution1.js
+++ Binary-Tree/104/solution1.js
@@ -1,3 +1,13 @@
+/**
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * 
+ * 104. 二叉树的最大深度
+ * 
+ * Easy
+ * 
+ * 88ms 90.87%
+ * 37.1mb 46.80%
+ */
 const maxDepth = root => {
   if (!root) {
     return 0
