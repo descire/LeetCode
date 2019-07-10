15f599590add31f912be3ed6c5816cf63ee35ab1
descire <daiqingyunc@gmail.com>
2019-07-10 23:15:43 +0800

    feat(binary-tree): :sparkles:递归解决700

--- Binary-Tree/700/solution1.js
+++ Binary-Tree/700/solution1.js
@@ -1,3 +1,13 @@
+/**
+ * 700. 二叉搜索树中的搜索
+ * 
+ * https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+ * 
+ * Easy
+ * 
+ * 116ms 90.57%
+ * 42.3mb 15.47%
+ */
 const searchBST = (root, val) => {
   if (!root) {
     return null
