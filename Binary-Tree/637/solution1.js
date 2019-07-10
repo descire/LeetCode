58c5d17d249733236f18b2a9c2a9ee7235ebd3e4
descire <daiqingyunc@gmail.com>
2019-07-11 00:26:12 +0800

    feat(binary-tree): :sparkles:递归解决637

--- Binary-Tree/637/solution1.js
+++ Binary-Tree/637/solution1.js
@@ -1,21 +1,31 @@
+/**
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+ * 
+ * 637. 二叉树的层平均值
+ * 
+ * Easy
+ * 
+ * 92ms 98.61%
+ * 38.2mb 50.00%
+ * 
+ */
 const averageOfLevels = root => {
   const ans = []
-  help(root, 0)
+  levelOrder(root, 0, ans)
   for (let i = 0; i < ans.length; i++) {
     const item = ans[i]
     const max = item.length
     ans[i] = item.reduce((pre, next) => pre + next, 0) / max
   }
   return ans
-  function help (root, level) {
-    if (!root) {
-      return
-    }
-    if (!ans[level]) {
-      ans[level] = []
-    }
-    ans[level].push(root.val)
-    help(root.left, level + 1)
-    help(root.right, level + 1)
+}
+
+function levelOrder (root, level, ans) {
+  if (!root) {
+    return
   }
+  ans[level] || (ans[level] = [])
+  ans[level].push(root.val)
+  levelOrder(root.left, level + 1, ans)
+  levelOrder(root.right, level + 1, ans)
 }
