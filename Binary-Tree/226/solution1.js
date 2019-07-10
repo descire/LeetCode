83fe404edee795d50cf58408f97ae33290a024e6
descire <daiqingyunc@gmail.com>
2019-07-10 23:04:00 +0800

    feat(binary-tree): :sparkles:递归解决226

--- Binary-Tree/226/solution1.js
+++ Binary-Tree/226/solution1.js
@@ -1,10 +1,22 @@
+/**
+ * 226. 翻转二叉树
+ * 
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ * 
+ * Easy
+ * 
+ * 68ms 98.02%
+ * 
+ * 34.2mb 5.92%
+ */
 const invertTree = (root) => {
   if (!root) {
-    return root
+    return null
   }
-  const temp = root.left
-  root.left = root.right
-  root.right = temp
+  // 交换左右子树
+  [root.left, root.right] = [root.right, root.left]
+
+  // 递归处理左右子树
   invertTree(root.left)
   invertTree(root.right)
   return root
