43624138e2c15c2f849a7f4089626a738cfbf2f9
descire <daiqingyunc@gmail.com>
2019-07-11 01:07:25 +0800

    feat(binary-tree): :sparkles:递归处理538

--- Binary-Tree/538/index.js
+++ Binary-Tree/538/index.js
@@ -1,8 +1,19 @@
+/**
+ * 538. 把二叉搜索树转换为累加树
+ * 
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ * 
+ * Easy
+ * 
+ * 132ms 84.93%
+ * 40.2mb 41.38%
+ */
 const convertBST = root => {
   let sum = 0
-  dfs(root)
+  dfs(root, 0)
   return root
-  function dfs (root) {
+
+  function dfs(root) {
     if (!root) {
       return
     }
