8ed0681a8adcf5b858ebde052720e286b8981bda
descire <daiqingyunc@gmail.com>
2019-07-11 11:49:23 +0800

    feat(binary-tree): :sparkles:递归解决100

--- Binary-Tree/100/solution1.js
+++ Binary-Tree/100/solution1.js
@@ -1,3 +1,15 @@
+/**
+ * https://leetcode-cn.com/problems/same-tree/
+ * 
+ * 100. 相同的树
+ * 
+ * Easy
+ * 
+ * 递归的特性
+ * 
+ * 80ms 73.52%
+ * 33.8% 19.32%
+ */
 const isSameTree = function (p, q) {
   if (p === null && q === null) {
     return true
