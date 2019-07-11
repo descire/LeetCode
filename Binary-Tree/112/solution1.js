64beb48397b7bc85fc49d3b80f0972a3abb3ffc2
descire <daiqingyunc@gmail.com>
2019-07-11 15:27:22 +0800

    feat(binary-tree): :sparkles:递归解决112

--- Binary-Tree/112/solution1.js
+++ Binary-Tree/112/solution1.js
@@ -1,3 +1,13 @@
+/**
+ *  https://leetcode-cn.com/problems/path-sum/
+ * 
+ * 112. 路径总和
+ * 
+ * Easy
+ * 
+ * 96ms 84.76%
+ * 37.5mb 8.05%
+ */
 const hasPathSum = (root, sum) => {
   if (!root) {
     return false
