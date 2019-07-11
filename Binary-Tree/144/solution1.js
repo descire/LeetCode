fc996872f371974a38680accf940ff6c9ae74387
descire <daiqingyunc@gmail.com>
2019-07-11 21:48:01 +0800

    feat(binary-tree): :sparkles:递归前序遍历144

--- Binary-Tree/144/solution1.js
+++ Binary-Tree/144/solution1.js
@@ -1,13 +1,24 @@
+/**
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ * 
+ * 144. 二叉树的前序遍历
+ * 
+ * Medium
+ * 
+ * 56ms 99.52%
+ * 34mb 7.26%
+ */
 const preorderTraversal = root => {
   const ans = []
-  preOrder(root)
+  preOrder(root, ans)
   return ans
-  function preOrder (root) {
-    if (!root) {
-      return
-    }
-    ans.push(root.val)
-    preOrder(root.left)
-    preOrder(root.right)
+}
+
+function preOrder (root, ans) {
+  if (!root) {
+    return
   }
+  ans.push(root.val)
+  preOrder(root.left, ans)
+  preOrder(root.right, ans)
 }
