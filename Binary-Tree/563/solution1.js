73cb9ac265dcbd43a6210020faf8cbc65a12b4c2
descire <daiqingyunc@gmail.com>
2019-07-11 12:32:13 +0800

    feat(binary-tree): :sparkles:后序遍历解决563

--- Binary-Tree/563/solution1.js
+++ Binary-Tree/563/solution1.js
@@ -1,15 +1,27 @@
+/**
+ * 
+ * https://leetcode-cn.com/problems/binary-tree-tilt/
+ * 
+ * 563. 二叉树的坡度
+ * 
+ * Easy
+ * 
+ * 108ms 73.33%
+ * 38.3mb 26.67%
+ */
 const findTilt = root => {
+  let sum = 0
+  postOrder(root)
+  return sum
+
   function postOrder (root) {
     if (!root) {
       return 0
     }
-    const left = postOrder(root.left)
-    const right = postOrder(root.right)
+    const leftSum = postOrder(root.left)
+    const rightSum = postOrder(root.right)
 
-    sum += Math.abs(left - right)
-    return root.val + left + right
+    sum += Math.abs(leftSum - rightSum)
+    return root.val + leftSum + rightSum
   }
-  let sum = 0
-  postOrder(root)
-  return sum
 }
