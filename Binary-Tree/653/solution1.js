d3a5ca2e2e937a595139ce7c81a7aad71c0a72b3
descire <daiqingyunc@gmail.com>
2019-07-11 11:59:28 +0800

    feat(binary-tree): :sparkles:前序遍历解决653

--- Binary-Tree/653/solution1.js
+++ Binary-Tree/653/solution1.js
@@ -1,3 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+ * 
+ * 653. 两数之和 IV - 输入 BST
+ * 
+ * Easy
+ * 
+ * 108ms 100%
+ * 41.6mb 61.76%
+ */
+
 const findTarget = (root, k) => {
   let ans = false
   const s = new Set()
