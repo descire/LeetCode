824c172f5ad233df60570ffcbdb926a3b72fa055
descire <daiqingyunc@gmail.com>
2019-07-11 18:17:06 +0800

    feat(binary-tree): :sparkles:递归解决572

--- Binary-Tree/572/solution1.js
+++ Binary-Tree/572/solution1.js
@@ -1,3 +1,13 @@
+/**
+ * https://leetcode-cn.com/problems/subtree-of-another-tree/
+ * 
+ * 572. 另一个树的子树
+ * 
+ * Easy
+ * 
+ * 104ms 100.00%
+ * 38.7mb 62.79%
+ */
 const isSubtree = (s, t) => {
 
   if (!s) {
@@ -9,17 +19,17 @@ const isSubtree = (s, t) => {
   }
 
   return isSubtree(s.left, t) || isSubtree(s.right, t)
+}
 
-  function isSame (s, t) {
-    if (!s && !t) {
-      return true
-    }
-    if (!s || !t) {
-      return false
-    }
-    if (s.val !== t.val) {
-      return false
-    }
-    return isSame(s.left, t.left) && isSame(s.right, t.right)
+function isSame (s, t) {
+  if (!s && !t) {
+    return true
   }
+  if (!s || !t) {
+    return false
+  }
+  if (s.val !== t.val) {
+    return false
+  }
+  return isSame(s.left, t.left) && isSame(s.right, t.right)
 }
