f6e3bf6da49aafa74ae83c4d544d01e5e9a03ef3
descire <daiqingyunc@gmail.com>
2019-07-11 12:26:04 +0800

    feat(binary-tree): :sparkles:递归解决110

--- Binary-Tree/110/solution1.js
+++ Binary-Tree/110/solution1.js
@@ -1,18 +1,27 @@
-const isBalanced = function (root) {
-
+/**
+ * https://leetcode-cn.com/problems/balanced-binary-tree/
+ * 
+ * 110. 平衡二叉树
+ * 
+ * Easy
+ * 
+ * 96ms 95.09%
+ * 37.9mb 15.84%
+ */
+const isBalanced = root => {
   if (!root) {
     return true
   }
 
-  const left = height(root.left)
-  const right = height(root.right)
+  const leftHeight = getHeight(root.left)
+  const rightHeight = getHeight(root.right)
 
-  return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+  return Math.abs(leftHeight - rightHeight) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+}
 
-  function height (root) {
-    if (!root) {
-      return 0
-    }
-    return Math.max(height(root.left), height(root.right)) + 1
+function getHeight(root) {
+  if (!root) {
+    return 0
   }
+  return Math.max(getHeight(root.left), getHeight(root.right)) + 1
 }
