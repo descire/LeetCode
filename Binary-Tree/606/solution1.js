fa7b4dbf715e5815f1a61fd123dc4270acfcb337
descire <daiqingyunc@gmail.com>
2019-07-11 12:11:17 +0800

    feat(binary-tree): :sparkles:递归解决606

--- Binary-Tree/606/solution1.js
+++ Binary-Tree/606/solution1.js
@@ -1,3 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/construct-string-from-binary-tree/
+ * 
+ * 606. 根据二叉树创建字符串
+ * 
+ * Easy
+ * 
+ * 112ms 74.47%
+ * 37.5mb 100.00%
+ * 
+ */
 const tree2str = t => {
   if (!t) {
     return ''
