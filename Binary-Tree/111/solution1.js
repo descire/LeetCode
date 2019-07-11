a2b1d5d65eb539ef9ded0d21063d55b286e27384
descire <daiqingyunc@gmail.com>
2019-07-11 18:22:31 +0800

    feat(binary-tree): :sparkles:递归解决111

--- Binary-Tree/111/solution1.js
+++ Binary-Tree/111/solution1.js
@@ -1,3 +1,13 @@
+/**
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+ * 
+ * 111. 二叉树的最小深度
+ * 
+ * Easy
+ * 
+ * 84ms 96.55%
+ * 37.5mb 22.22%
+ */
 const minDepth = function (root) {
   if (!root) {
     return 0
