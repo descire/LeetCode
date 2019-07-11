c936f03e2df5dd2fac2094821aca38e393c80d83
descire <daiqingyunc@gmail.com>
2019-07-11 13:25:51 +0800

    feat(binary-tree): :sparkles:递归解决101

--- Binary-Tree/101/solution1.js
+++ Binary-Tree/101/solution1.js
@@ -1,15 +1,27 @@
+/**
+ * https://leetcode-cn.com/problems/symmetric-tree/
+ * 
+ * 101. 对称二叉树
+ * 
+ * Easy
+ * 
+ * 84ms 92.13%
+ * 35.4mb 58.84%
+ */
+
 const isSymmetric = function (root) {
-  if (root === null) {
+  if (!root) {
     return true
   }
-  return isSymmetricHelper(root.left, root.right)
-  function isSymmetricHelper (left, right) {
-    if (left === null && right === null) {
-      return true
-    }
-    if (left === null || right === null) {
-      return false
-    }
-    return (left.val === right.val && isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left))
+  return _isSymmetric(root.left, root.right)
+}
+
+function _isSymmetric (left, right) {
+  if (left === null && right === null) {
+    return true
   }
+  if (left === null || right === null) {
+    return false
+  }
+  return (left.val === right.val && _isSymmetric(left.left, right.right) && _isSymmetric(left.right, right.left))
 }
