617112f28ee978ccd8d437e6ac0b8a8c404db659
descire <daiqingyunc@gmail.com>
2019-07-13 22:44:49 +0800

    feat(binary-tree): :sparkles:DFS解决94中序遍历

--- Binary-Tree/94/solution1.js
+++ Binary-Tree/94/solution1.js
@@ -1,13 +1,26 @@
+/**
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ * 
+ * 94. 二叉树的中序遍历
+ * 
+ * Medium
+ * 
+ * 84ms 64.76%
+ * 33.6mb 71.27%
+ * 
+ * DFS
+ */
 const inorderTraversal = root => {
   const ans = []
-  inorder(root)
+  dfs(root, ans)
   return ans
-  function inorder (root) {
-    if (!root) {
-      return
-    }
-    inorder(root.left)
-    ans.push(root.val)
-    inorder(root.right)
+}
+
+function dfs(root, ans) {
+  if (!root) {
+    return
   }
+  dfs(root.left, ans)
+  ans.push(root.val)
+  dfs(root.right, ans)
 }
