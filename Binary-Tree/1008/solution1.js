28749b1ef8cda98f63a30b7443f6c0526ed915e8
descire <daiqingyunc@gmail.com>
2019-07-16 00:22:07 +0800

    docs(binary-tree): :memo:完成二叉树 Medium 二叉树的构造

--- Binary-Tree/1008/solution1.js
+++ Binary-Tree/1008/solution1.js
@@ -10,24 +10,24 @@
  * 76ms 90.91%
  * 34.7mb 42.42%
  */
-const bstFromPreorder = perorder => {
+const bstFromPreorder = preorder => {
   // 终止条件
-  if (!perorder.length) {
+  if (!preorder.length) {
     return null
   }
-  const root = new TreeNode(perorder[0])
+  const root = new TreeNode(preorder[0])
 
   // 寻找左右子树的分割点
   let index = 0
-  const max = perorder.length
+  const max = preorder.length
   for (let i = 0; i < max; i++) {
-    if (perorder[0] >= perorder[i]) {
+    if (preorder[0] >= preorder[i]) {
       index = i
       continue
     }
     break
   }
-  root.left = bstFromPreorder(perorder.slice(1, index + 1))  
-  root.right = bstFromPreorder(perorder.slice(index + 1))
+  root.left = bstFromPreorder(preorder.slice(1, index + 1))  
+  root.right = bstFromPreorder(preorder.slice(index + 1))
   return root
 }
