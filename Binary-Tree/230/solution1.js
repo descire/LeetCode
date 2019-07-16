e497afad36c72fbfd958b8007e3d9c2a3e9c7075
descire <daiqingyunc@gmail.com>
2019-07-16 23:39:49 +0800

    feat(binary-tree): :sparkles:中序遍历解决230

--- Binary-Tree/230/solution1.js
+++ Binary-Tree/230/solution1.js
@@ -1,17 +1,27 @@
+/**
+ * 230. 二叉搜索树中第K小的元素
+ * 
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+ * 
+ * Medium
+ * 
+ * 96ms 90.91%
+ * 39.3mb 51.39%
+ */
 const kthSmallest = (root, k) => {
-  let index = 0
   let ans = null
+  let count = 0
   if (!root) {
     return ans
   }
   inOrder(root)
   return ans
-  function inOrder (root) {
+  function inOrder(root) {
     if (!root) {
       return
     }
     inOrder(root.left)
-    if (++index === k) {
+    if (++count === k) {
       ans = root.val
       return
     }
