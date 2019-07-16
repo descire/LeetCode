a6cb0213b36a6ac0ab78869108280d1f23f66298
descire <daiqingyunc@gmail.com>
2019-07-16 23:01:41 +0800

    feat(binary-tree): :sparkles:递归解决814

--- Binary-Tree/814/solution1.js
+++ Binary-Tree/814/solution1.js
@@ -1,9 +1,15 @@
+/**
+ * https://leetcode-cn.com/problems/binary-tree-pruning/
+ * 
+ * 814. 二叉树剪枝
+ * 
+ * Medium
+ */
 const pruneTree = root => {
   if (!root) {
     return null
   }
-
   root.left = pruneTree(root.left)
   root.right = pruneTree(root.right)
-  return (root.val == 1 || root.left != null || root.right != null) ? root : null
+  return (root.val === 1 || root.left !== null || root.right !== null) ? root : null
 }
