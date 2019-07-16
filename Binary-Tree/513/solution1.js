fc1519a1e63134242d83b320edab71535ba66697
descire <daiqingyunc@gmail.com>
2019-07-16 23:29:35 +0800

    feat(binary-tree): :sparkles:递归解决513

--- Binary-Tree/513/solution1.js
+++ Binary-Tree/513/solution1.js
@@ -1,24 +1,33 @@
+/**
+ * https://leetcode-cn.com/problems/find-bottom-left-tree-value/
+ * 
+ * 513. 找树左下角的值
+ * 
+ * Medium
+ * 
+ * 80ms 100.00%
+ * 37.1mb 100.00%
+ */
 const findBottomLeftValue = root => {
   if (!root) {
     return null
   }
-  let currentLevel = 0
-  let ans = root.val
 
-  inOrder(root, 0)
-
+  let ans = 0
+  let currentLevel = -1
+  levelOrder(root, 0)
   return ans
 
-  function inOrder (root, level) {
+  function levelOrder(root, level) {
     if (!root) {
       return
     }
-
     if (level > currentLevel) {
       currentLevel = level
       ans = root.val
     }
-    inOrder(root.left, level + 1)
-    inOrder(root.right, level + 1)
+    levelOrder(root.left, level + 1)
+    levelOrder(root.right, level + 1)
   }
 }
+
