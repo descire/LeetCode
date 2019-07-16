4df6c3c41340042aea8293705c4d96181fb5669b
descire <daiqingyunc@gmail.com>
2019-07-16 23:33:23 +0800

    feat(binary-tree): :sparkles:递归解决701

--- Binary-Tree/701/solution1.js
+++ Binary-Tree/701/solution1.js
@@ -1,3 +1,13 @@
+/**
+ * https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+ * 
+ * 701. 二叉搜索树中的插入操作
+ * 
+ * Medium
+ * 
+ * 168ms 66.67%
+ * 42.3mb 9.09%
+ */
 const insertIntoBST = (root, val) => {
   if (!root) {
     return new TreeNode(val)
