ce9bf758db6406282afee9feb572f215eb97bbe2
descire <daiqingyunc@gmail.com>
2019-07-17 21:44:54 +0800

    feat(binary-tree): :sparkles:递归处理96

--- Binary-Tree/96/solution1.js
+++ Binary-Tree/96/solution1.js
@@ -1,6 +1,16 @@
+/**
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/
+ * 
+ * 96. 不同的二叉搜索树
+ * 
+ * Medium
+ * 
+ * 64ms 96.00%
+ * 33.7mb 40.00%
+ */
 const numTrees = n => {
   const dp = new Array(n + 1).fill(0)
-  dp[0] = 1
+  dp[0] = 1 
   dp[1] = 1
   for (let i = 2; i <= n; i++) {
     for (let j = 0; j < i; j++) {
