e71b499dedd9cce36d40036afd80f0f8898673f9
descire <daiqingyunc@gmail.com>
2019-08-18 12:27:37 +0800

    refactor(Binary-Tree): :art:优化代码

--- Binary-Tree/1145/solution1.js
+++ Binary-Tree/1145/solution1.js
@@ -8,35 +8,48 @@
  * 64ms 97.14%
  * 34.1mb 100.00%
  */
+
+// 父节点方向
+const PARENT_DIRECTION = 0;
+// 左子树方向
+const LEFT_DIRECTION = 1;
+// 右子树方向
+const RIGHT_DIRECTION = 2;
+
 const btreeGameWinningMove = (root, n, x) => {
   let one = 0;
   let two = 0;
   let three = 0;
-  dfs(root, 0);
+  dfs(root, PARENT_DIRECTION);
 
   if (canWin(one, two + three + 1) || canWin(two, one + three + 1) || canWin(three, one + two + 1)) {
     return true;
   }
   return false;
 
-  function dfs(root, flag) {
+  function dfs(root, direction) {
     if (!root) {
       return;
     }
     if (root.val === x) {
-      dfs(root.left, 1);
-      dfs(root.right, 2);
-    } else {
-      if (flag === 0) {
-        one++;
-      } else if (flag === 1) {
-        two++;
-      } else if (flag === 2) {
-        three++;
-      }
-      dfs(root.left, flag);
-      dfs(root.right, flag);
+      dfs(root.left, LEFT_DIRECTION);
+      dfs(root.right, RIGHT_DIRECTION);
+      return;
     }
+
+    switch(direction) {
+      case PARENT_DIRECTION:
+        one++;
+        break;
+      case LEFT_DIRECTION:
+        two++;
+        break;
+      case RIGHT_DIRECTION:
+        three++;
+        break;
+    }
+    dfs(root.left, direction);
+    dfs(root.right, direction);
   }
 }
 
