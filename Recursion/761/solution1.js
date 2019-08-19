db02ba24ca89384514cd2b11cbfdffc837be268f
descire <daiqingyunc@gmail.com>
2019-08-19 22:17:15 +0800

    feat(Recursion): :sparkles:761未完待续

---
+++ Recursion/761/solution1.js
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode-cn.com/problems/special-binary-string/
+ * 
+ * 761. 特殊的二进制序列
+ * 
+ * Hard
+ * 
+ * 使得字符最大，也就是尽量将“1”放在左边，“0”放在右边。
+ */
+const makeLargestSpecial = S => {
+
+}
