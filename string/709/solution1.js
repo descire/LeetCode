f78ea6f8ddae8cfefe14e9900e950243dbd8a829
descire <daiqingyunc@gmail.com>
2019-09-01 21:52:03 +0800

    feat(string): :sparkles:利用JS内置函数解决709

---
+++ string/709/solution1.js
@@ -0,0 +1,13 @@
+/**
+ * 709. 转换成小写字母
+ * 
+ * https://leetcode-cn.com/problems/to-lower-case/
+ * 
+ * Easy
+ * 
+ * 64ms 95.27%
+ * 33.5mb 79.35%
+ */
+const toLowerCase = (str) => {
+  return str.toLowerCase();
+}
