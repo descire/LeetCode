6369a15b1edadd2867df1df7196c2b79a09eb7b5
descire <daiqingyunc@gmail.com>
2020-02-23 21:43:30 +0800

    docs(5169): :memo:添加说明

--- Rank/177/5169/solution1.js
+++ Rank/177/5169/solution1.js
@@ -1,13 +1,16 @@
+/**
+ * https://leetcode-cn.com/problems/number-of-days-between-two-dates/
+ * 
+ * 5169. 日期之间隔几天
+ * 
+ * Easy
+ */
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
 const daysBetweenDates = (date1, date2) => {
   const [y1, m1, d1] = date1.split('-');
   const [y2, m2, d2] = date2.split('-');
   const time1 = new Date(y1, m1 - 1, d1, 0, 0, 0);
   const time2 = new Date(y2, m2 - 1, d2, 0, 0, 0);
-  const timestamp = Math.abs(time2 - time1);
-
-  return timestamp / (24 * 60 * 60 * 1000);
+  return Math.abs(time2 - time1) / ONE_DAY;
 }
-
-
-console.log(daysBetweenDates('2020-01-15', '2019-12-31'))
-console.log(daysBetweenDates('2019-06-29', '2019-06-30'))
