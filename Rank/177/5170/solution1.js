39f973ba8482433f29c4cd54fde8e20b686e3b83
descire <daiqingyunc@gmail.com>
2020-02-23 22:06:29 +0800

    docs(5170): :memo:添加说明

--- Rank/177/5170/solution1.js
+++ Rank/177/5170/solution1.js
@@ -1,6 +1,17 @@
+/**
+ * https://leetcode-cn.com/problems/validate-binary-tree-nodes/
+ * 
+ * 5170. 验证二叉树
+ * 
+ * Medium
+ * 
+ * 76ms 100.00%
+ * 39.7mb 100.00%
+ */
 const validateBinaryTreeNodes = (n, leftChild, rightChild) => {
   const record = new Set();
   const result = help(0, leftChild, rightChild, record);
+  // 是否为一棵树
   const count = record.size;
   return (result && count === n - 1);
 }
@@ -9,6 +20,7 @@ function help(n, leftChild, rightChild, record) {
   const leftItem = leftChild[n];
   const rightItem = rightChild[n];
   if (record.has(leftItem) || record.has(rightItem)) {
+    // 存在非法的回路情况
     return false;
   }
 
@@ -27,5 +39,3 @@ function help(n, leftChild, rightChild, record) {
 
   return a && b;
 }
-
-console.log(validateBinaryTreeNodes(4, [1,-1,3,-1], [2,-1,-1,-1]))
