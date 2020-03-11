1947635d5442e415e5f47449fda945292c55bd0f
descire <daiqingyunc@gmail.com>
2020-03-11 23:41:38 +0800

    feat(1013): :sparkles:解决1013

---
+++ one-question-per-day/20200311/solution1.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
+ * 
+ * 1013. 将数组分成和相等的三个部分
+ * 
+ * Easy
+ * 
+ * 5216ms 5.74%
+ * 39.5mb 98.15%
+ * 
+ * 时间复杂度 O(n^2)
+ */
+const canThreePartsEqualSum = A => {
+  const max = A.length;
+  if (max < 3) {
+    return false;
+  }
+  const sum = A.reduce((a, b) => a + b, 0);
+
+  if (sum % 3 !== 0) {
+    return false;
+  }
+
+  let firstPartSum = 0;
+  for (let i = 0; i < max - 2; i++) {
+    firstPartSum += A[i];
+    let secondPartSum = 0;
+    for (let j = i + 1; j < max - 1; j++) {
+      secondPartSum += A[j];
+      if (firstPartSum === secondPartSum && 3 * secondPartSum === sum) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
