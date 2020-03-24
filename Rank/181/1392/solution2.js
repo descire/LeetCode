dad6587a9934f1d22753da04fcff2a09096256fc
descire <daiqingyunc@gmail.com>
2020-03-25 00:31:25 +0800

    docs(1392): :docs:添加注释

--- Rank/181/1392/solution2.js
+++ Rank/181/1392/solution2.js
@@ -18,12 +18,13 @@ const longestPrefix = s => {
 }
 
 function makeNext(str) {
-  let index = 1;
-  let k = 0;
+  let index = 1; // 当前子字符串的结束下标
+  let k = 0; // 最长公共前后缀的长度
   const len = str.length;
   const next = Array(len).fill(0);
   for (;index < len; index += 1) {
     while (k > 0 && str[index] !== str[k]) {
+      // 递归向前查找最大公共子串
       k = next[k - 1];
     }
     if (str[index] === str[k]) {
