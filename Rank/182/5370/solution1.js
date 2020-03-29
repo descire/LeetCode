fbcc06d380fdeae1954e7a46209828ec27bf7ef9
descire <daiqingyunc@gmail.com>
2020-03-29 16:41:26 +0800

    feat(rank): :sparkles:整理 5370

--- Rank/182/5370/solution1.js
+++ Rank/182/5370/solution1.js
@@ -1,40 +1,31 @@
-
+/**
+ * https://leetcode-cn.com/problems/design-underground-system/submissions/
+ * 
+ * Medium
+ * 
+ * 5370. 设计地铁系统
+ * 
+ * 300ms 100.00%
+ * 59.3mb 100.00%
+ */
 var UndergroundSystem = function() {
-  this.inMap = new Map();
+  this.record = new Map();
   this.resultMap = new Map();
 };
 
-/** 
- * @param {number} id 
- * @param {string} stationName 
- * @param {number} t
- * @return {void}
- */
 UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
-  this.inMap.set(id, [stationName, t]);
+  this.record.set(id, [stationName, t]);
 };
 
-/** 
- * @param {number} id 
- * @param {string} stationName 
- * @param {number} t
- * @return {void}
- */
 UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
-  const [startStation, startT] = this.inMap.get(id);
+  const [startStation, startT] = this.record.get(id);
   const time = t - startT;
   if (!this.resultMap.has(`${startStation}-${stationName}`)) {
     this.resultMap.set(`${startStation}-${stationName}`, []);
   }
   this.resultMap.get(`${startStation}-${stationName}`).push(time);
-  this.inMap.delete(id);
+  this.record.delete(id);
 };
-
-/** 
- * @param {string} startStation 
- * @param {string} endStation
- * @return {number}
- */
 UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
   const list = this.resultMap.get(`${startStation}-${endStation}`);
   const max = list.length;
