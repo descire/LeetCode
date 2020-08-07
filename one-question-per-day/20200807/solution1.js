3959825e89bd10d6e1852d52fc63279939a75020
descire <daiqingyunc@gmail.com>
2020-08-07 21:23:55 +0800

    feat(每日一题): :sparkles:优化解法

--- one-question-per-day/20200807/solution1.js
+++ one-question-per-day/20200807/solution1.js
@@ -1,27 +1,12 @@
 var isSameTree = function(p, q) {
-    const x = [];
-    const y = [];
-    dfs(p, x);
-    dfs(q, y);
-    const xlen = x.length;
-    const ylen = y.length;
-    if (xlen !== ylen) {
-      return false;
-    }
-    for (let i = 0; i < xlen; i++) {
-      if (x[i] !== y[i]) {
-        return false;
-      }
-    }
+  if (!p && !q) {
     return true;
-};
-
-function dfs(root, record){ 
-  if (!root) {
-    record.push(null);
-    return;
   }
-  record.push(root.val);
-  dfs(root.left, record);
-  dfs(root.right, record);
-}
+  if (!p || !q) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
