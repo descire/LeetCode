f25ea38365a8a3f841e32d086b225e4a6343f36b
descire <daiqingyunc@gmail.com>
2021-06-10 23:09:10 +0800

    feat(518): :sparkles:动态规划

---
+++ array/518/solution1.js
@@ -0,0 +1,11 @@
+const change = (amount, coins) => {
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+  return dp[amount];
+}
