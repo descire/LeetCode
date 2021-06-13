41204366214a035c6ca2ad52271730ba8b5ce3d2
descire <daiqingyunc@gmail.com>
2021-06-13 22:23:45 +0800

    feat(278): :sparkles:二分法

---
+++ array/278/solution2.js
@@ -0,0 +1,20 @@
+const solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let start = 1;
+    let end = n;
+
+    while (start < end) {
+      const mid = Math.floor(start + (end - start) / 2);
+      if (isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  };
+};
