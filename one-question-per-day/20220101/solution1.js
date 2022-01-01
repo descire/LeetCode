a260e214628f56db558fcfe42a9b4953c7a8529e
descire <daiqingyunc@gmail.com>
2022-01-01 11:42:35 +0800

    feat(): 每日一题

---
+++ one-question-per-day/20220101/solution1.js
@@ -0,0 +1,26 @@
+/**
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(n*m)
+ * @param {*} original 
+ * @param {*} m s
+ * @param {*} n 
+ * @returns 
+ */
+const construct2DArray = function(original, m, n) {
+    const maxLength = original.length;
+    if (m * n !== maxLength) {
+        return [];
+    }
+
+    const ans = [];
+    for (let i = 0; i < maxLength; i++) {
+        const index = Math.floor(i / n);
+        if (!ans[index]) {
+            ans[index] = [];
+        }
+
+        ans[index].push(original[i]);
+    }
+
+    return ans;
+};
