4067ad647ffdf9e257dc5563d307d66510cdc37e
descire <daiqingyunc@gmail.com>
2022-01-05 22:18:29 +0800

    feat(): 每日一题

---
+++ one-question-per-day/20220105/solution1.js
@@ -0,0 +1,23 @@
+
+const replaceStr = (pre, next) => {
+    const str = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== pre && str[i] !== next) {
+            return str[i];
+        }
+    }
+}
+const modifyString = function(s) {
+    const str = 'abcdefghijklmnopqrstuvwxyz';
+
+    const ans = s.split('');
+
+    for (let i = 0; i < ans.length; i++) {
+        if (ans[i] === '?') {
+            // 进行替换操作
+            ans[i] = replaceStr(ans[i - 1], ans[i + 1]);
+        }
+    }
+
+    return ans.join('');
+};
