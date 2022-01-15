b84f33e95359d4d849428abb75d60d3706c2ee9f
descire <daiqingyunc@gmail.com>
2022-01-15 23:55:31 +0800

    feat(数据结构入门): 数组

--- array/53/index.js
+++ array/53/index.js
@@ -1,11 +1,20 @@
+/**
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(1)
+ * 
+ * 单独一段 还是连续一段
+ * 
+ * max = Number.max(连续的一段，新的起点)
+ * @param {*} nums 
+ */
 const maxSubArray = (nums) => {
-  let preSum = 0;
-  let ans = nums[0];
+  let ans = 0
+  let pre = nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
-    preSum = Math.max(preSum + nums[i], nums[i]);
-    ans = Math.max(ans, preSum);
-  };
+  for (let i = 1; i < nums.length; i++) {
+    pre = Math.max(pre + nums[i], nums[i]);
+    ans = Math.max(ans, pre);
+  }
 
   return ans;
 }
