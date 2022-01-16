f24bce149930fb8283398e1a576eca149ae460a0
descire <daiqingyunc@gmail.com>
2022-01-16 09:39:41 +0800

    feat(数据结构入门): 直接排序解法

--- array/88/index.js
+++ array/88/index.js
@@ -1,25 +1,15 @@
+/**
+ * 直接合并两个数组，然后排序处理
+ * 
+ * 时间复杂度 O((m + n)log(m+n))
+ * 空间复杂度 O(m + n)
+ * @param {*} nums1 
+ * @param {*} m 
+ * @param {*} nums2 
+ * @param {*} n 
+ * @returns 
+ */
 var merge = function(nums1, m, nums2, n) {
-  let p1 = 0;
-  let p2 = 0;
-  const ans = [];
-  let current = 0;
-  while (p1 < m || p2 < n) {
-    if (p1 === m) {
-      current = nums2[p2++];
-    } else if (p2 === n) {
-      current = nums1[p1++];
-    } else if (nums1[p1] < nums2[p2]) {
-      current = nums1[p1++];
-    } else {
-      current = nums2[p2++];
-    }
-
-    ans[p1 + p2 - 1] = current;
-  }
-
-  for (let i = 0; i < m + n; i++) {
-    nums1[i] = ans[i];
-  }
-
-  return nums1;
+  nums1.splice(m, nums1.length - m, ...nums2);
+  return nums1.sort((a, b) => a - b);
 };
