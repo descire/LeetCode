4b241063310f0f2a23a6158e0f80902e4ccd5517
descire <daiqingyunc@gmail.com>
2022-01-21 19:52:05 +0800

    feat(数据结构入门): 141

--- Linked-List/141/solution1.js
+++ Linked-List/141/solution1.js
@@ -1,21 +1,17 @@
 /**
- * https://leetcode-cn.com/problems/linked-list-cycle/
- * 
- * 141. 环形链表
- * 
- * Easy
- * 
- * 88ms 27.14%
- * 38.2mb 5.05%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const hasCycle = head => {
   const record = new Set();
+
   while (head) {
     if (record.has(head)) {
       return true;
     }
-    record.add(head)
+    record.add(head);
     head = head.next;
   }
+
   return false;
 }
--- Linked-List/141/solution2.js
+++ Linked-List/141/solution2.js
@@ -1,26 +1,24 @@
 /**
- * https://leetcode-cn.com/problems/linked-list-cycle/
- * 
- * 141. 环形链表
- * 
- * Easy
- * 
- * 68ms 96.93%
- * 37mb 24.04%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
  */
 const hasCycle = head => {
+
   if (!head || !head.next) {
     return false;
   }
-  let slowHead = head;
-  let fastHead = head.next;
 
-  while (slowHead !== fastHead) {
-    if (!fastHead || !fastHead.next) {
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (!fast || !fast.next) {
+      // 到达终点
       return false;
     }
-    slowHead = slowHead.next;
-    fastHead = fastHead.next.next;
+
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
   return true;
