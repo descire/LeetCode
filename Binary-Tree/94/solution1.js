1887d18e8dca55bd44a61ed1024635de6ed57562
descire <daiqingyunc@gmail.com>
2022-01-25 00:31:23 +0800

    feat(数据结构入门): 94

--- Binary-Tree/94/solution1.js
+++ Binary-Tree/94/solution1.js
@@ -1,26 +1,21 @@
 /**
- * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
- * 
- * 94. 二叉树的中序遍历
- * 
- * Medium
- * 
- * 84ms 64.76%
- * 33.6mb 71.27%
- * 
- * DFS
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
-const inorderTraversal = root => {
-  const ans = []
-  dfs(root, ans)
-  return ans
+const help = (root, ans) => {
+  if (!root) {
+    return;
+  }
+  // 左
+  help(root.left, ans);
+  // 中
+  ans.push(root.val);
+  // 右
+  help(root.right, ans);
 }
 
-function dfs(root, ans) {
-  if (!root) {
-    return
-  }
-  dfs(root.left, ans)
-  ans.push(root.val)
-  dfs(root.right, ans)
+const inorderTraversal = root => {
+  const ans = [];
+  help(root, ans);
+  return ans;
 }
