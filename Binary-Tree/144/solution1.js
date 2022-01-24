c52b3681c270686899fe2547721edfd9dc6adf72
descire <daiqingyunc@gmail.com>
2022-01-24 23:52:13 +0800

    feat(数据结构入门): 144

--- Binary-Tree/144/solution1.js
+++ Binary-Tree/144/solution1.js
@@ -1,27 +1,20 @@
 /**
- * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
- * 
- * 144. 二叉树的前序遍历
- * 
- * Medium
- * 
- * 56ms 99.52%
- * 34mb 7.26%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
-const preorderTraversal = root => {
-  const ans = []
-  preOrder(root, ans)
-  return ans
-}
-
-function preOrder (root, ans) {
+const help = (root, ans) => {
   if (!root) {
-    return
+    return;
   }
-  // 访问根
-  ans.push(root.val)
-  // 前序遍历左子树
-  preOrder(root.left, ans)
-  // 前序遍历右子树
-  preOrder(root.right, ans)
+
+  ans.push(root.val);
+  // 遍历左子树
+  help(root.left, ans);
+  // 遍历右子树
+  help(root.right, ans);
+}
+const preorderTraversal = root => {
+  const ans = [];
+  help(root, ans);
+  return ans;
 }
