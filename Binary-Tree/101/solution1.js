16507d3bb5472cf5daba38252aa905b667a0ef97
descire <daiqingyunc@gmail.com>
2022-01-26 00:10:33 +0800

    feat(数据结构入门): 101

--- Binary-Tree/101/solution1.js
+++ Binary-Tree/101/solution1.js
@@ -1,27 +1,23 @@
 /**
- * https://leetcode-cn.com/problems/symmetric-tree/
- * 
- * 101. 对称二叉树
- * 
- * Easy
- * 
- * 84ms 92.13%
- * 35.4mb 58.84%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
-
 const isSymmetric = function (root) {
   if (!root) {
-    return true
+    return true;
   }
-  return _isSymmetric(root.left, root.right)
+
+  return _isSymmetric(root.left, root.right);
 }
 
 function _isSymmetric (left, right) {
   if (left === null && right === null) {
-    return true
+    return true;
   }
+
   if (left === null || right === null) {
-    return false
+    return false;
   }
-  return (left.val === right.val && _isSymmetric(left.left, right.right) && _isSymmetric(left.right, right.left))
+
+  return (left.val === right.val && _isSymmetric(left.left, right.right) && _isSymmetric(left.right, right.left));
 }
