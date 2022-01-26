6e70d6c3b3d3f543303241b0b19a785ec69755c0
descire <daiqingyunc@gmail.com>
2022-01-27 01:27:42 +0800

    feat(数据结构入门): 226

--- Binary-Tree/226/solution1.js
+++ Binary-Tree/226/solution1.js
@@ -1,23 +1,17 @@
 /**
- * 226. 翻转二叉树
- * 
- * https://leetcode-cn.com/problems/invert-binary-tree/
- * 
- * Easy
- * 
- * 68ms 98.02%
- * 
- * 34.2mb 5.92%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const invertTree = (root) => {
   if (!root) {
-    return null
+    return null;
   }
-  // 交换左右子树
-  [root.left, root.right] = [root.right, root.left]
 
-  // 递归处理左右子树
-  invertTree(root.left)
-  invertTree(root.right)
-  return root
+  [root.left, root.right] = [root.right, root.left];
+
+  invertTree(root.left);
+
+  invertTree(root.right);
+
+  return root;
 }
