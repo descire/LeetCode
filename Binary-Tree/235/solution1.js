aa03e2b824c95c4984de1044b731e5a370af091f
descire <daiqingyunc@gmail.com>
2022-01-29 00:50:55 +0800

    feat(数据结构入门): 235

--- Binary-Tree/235/solution1.js
+++ Binary-Tree/235/solution1.js
@@ -1,24 +1,17 @@
 /**
- * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
- * 
- * 235. 二叉搜索树的最近公共祖先
- * 
- * Easy
- * 
- * 
- * 112ms 92.88%
- * 44.3mb 10.19%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const lowestCommonAncestor = (root, p, q) => {
   if (!root) {
-    return null
+    return null;
   }
 
   if (p.val < root.val && q.val < root.val) {
-    return lowestCommonAncestor(root.left, p, q)
+    return lowestCommonAncestor(root.left, p, q);
   } else if (p.val > root.val && q.val > root.val) {
-    return lowestCommonAncestor(root.right, p, q)
+    return lowestCommonAncestor(root.right, p, q);
   } else {
-    return root
+    return root;
   }
 }
