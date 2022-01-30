b322f94325a9456f0ba845c3cc3321c76ae52f83
descire <daiqingyunc@gmail.com>
2022-01-30 23:51:58 +0800

    feat(数据结构入门): 108

--- Binary-Tree/108/solution1.js
+++ Binary-Tree/108/solution1.js
@@ -1,11 +1,18 @@
-const sortedArrayToBST = function (nums) {
-  const max = nums.length
-  if (max === 0) {
-    return null
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(logn)
+ * 
+ */
+const help = (nums, start, end) => {
+  if (start > end) {
+    return null;
   }
-  const mid = Math.floor(max / 2)
-  const root = new TreeNode(nums[mid])
-  root.left = sortedArrayToBST(nums.slice(0, mid))
-  root.right = sortedArrayToBST(nums.slice(mid + 1, max))
-  return root
+  const mid = Math.floor((start + end + 1) / 2);
+  const root = new TreeNode(nums[mid]);
+  root.left = help(nums, start, mid - 1);
+  root.right = help(nums, mid + 1, end);
+  return root;
+}
+const sortedArrayToBST = function (nums) {
+  return help(nums, 0, nums.length - 1);
 }
