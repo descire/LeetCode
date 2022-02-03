ee821a002ad1faf137fc0b18c0a7145f470aa3c9
descire <daiqingyunc@gmail.com>
2022-02-03 20:29:10 +0800

    feat(523): 前缀和+哈希表

--- array/523/index.js
+++ array/523/index.js
@@ -1,20 +1,26 @@
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 const checkSubarraySum = (nums, k) => {
-  if (nums.length < 2) {
+  const length = nums.length;
+  if (length < 2) {
     return false;
   }
-  let currentSum = 0;
-  const record = new Map();
-  record.set(currentSum, -1);
 
-  for (let i = 0; i < nums.length; i++) {
-    currentSum = (currentSum + nums[i]) % k;
-    if (record.has(currentSum)) {
-      const preIndex = record.get(currentSum);
-      if (i - preIndex >= 2) {
-        return true;
-      }
-    } else {
-      record.set(currentSum, i);
+  const preSum = Array(length + 1).fill(0);
+
+  for (let i = 1; i <= length; i++) {
+    preSum[i] = preSum[i - 1] + nums[i - 1];
+  }
+
+  const record = new Set();
+
+  for (let i = 2; i <= length; i++) {
+    record.add(preSum[i - 2] % k);
+
+    if (record.has(preSum[i] % k)) {
+      return true;
     }
   }
   return false;
