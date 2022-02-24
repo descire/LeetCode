84685240df577c5ddec222a82fb8053b944071e0
descire <daiqingyunc@gmail.com>
2022-02-24 23:35:05 +0800

    faet(58): 数组基本操作

---
+++ array/58/solution1.js
@@ -0,0 +1,1 @@
+const lengthOfLastWord = s => s.split(' ').filter(item => item !== '').pop().length;
--- string/647/solution1.js
+++ string/647/solution1.js
@@ -1,29 +1,9 @@
 /**
- * https://leetcode.com/problems/palindromic-substrings/
+ * Manacher
  * 
- * 647. Palindromic Substrings
- * 
- * Medium
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const countSubstrings = s => {
-  const max = s.length - 1
 
-  const dp = []
-  let ans = 0
-  for (let i = max; i >= 0; i--) {
-    dp[i] = []
-    for (let j = i; j <= max; j++) {
-      if (i === j) {
-        dp[i][j] = true
-        ans++
-        continue
-      }
-      if (s[i] === s[j] && (j - i <= 1 ? true : dp[i + 1][j - 1])) {
-        dp[i][j] = true
-        ans++
-      }
-    }
-  }
-
-  return ans
 }
