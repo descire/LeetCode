91cbf1102070f011ea1827391ce03f5c4bc66ed0
descire <daiqingyunc@gmail.com>
2022-03-06 14:15:20 +0800

    feat(19): 遍历

--- Linked-List/19/solution1.js
+++ Linked-List/19/solution1.js
@@ -1,29 +1,25 @@
 /**
- * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
- * 
- * 19. 删除链表的倒数第N个节点
- * 
- * Medium
- * 
- * 60ms 94.99%
- * 33.9mb 51.22%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const removeNthFromEnd = (head, n) => {
   const dummyHead = new ListNode(-1);
   dummyHead.next = head;
-  // 记录链表的长度
   let currentHead = dummyHead.next;
-  let totalCount = 0;
-  while(currentHead) {
-    totalCount++;
+  let len = 0;
+  while (currentHead) {
+    len++;
     currentHead = currentHead.next;
   }
-  // 删除节点（题目中确保了n有效，所以不需要做健壮性处理）
-  let index = totalCount - n;
-  let prevHead = dummyHead;
-  while(index--) {
-    prevHead = prevHead.next;
+
+  currentHead = dummyHead;
+  let index = len - n;
+  while (index) {
+    index--;
+    currentHead = currentHead.next;
   }
-  prevHead.next = prevHead.next.next;
+
+  currentHead.next = currentHead.next.next;
+
   return dummyHead.next;
 }
