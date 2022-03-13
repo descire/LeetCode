c91870d01207791f1915d990b109d344ada14fc5
descire <daiqingyunc@gmail.com>
2022-03-14 00:39:22 +0800

    feat(61): 环形问题

--- Linked-List/61/solution1.js
+++ Linked-List/61/solution1.js
@@ -1,40 +1,36 @@
 /**
- * https://leetcode-cn.com/problems/rotate-list/
- * 
- * 61. 旋转链表
- * 
- * Medium
- * 
- * 68ms 95.08%
- * 35.8mb 10.71%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
  */
 const rotateRight = (head, k) => {
-  if (!head || k === 0) {
+
+  if (!head || !head.next) {
     return head;
   }
-  // 获取链表的长度 并且记录链表最后一个元素
-  let size = 0;
-  let lastHead = head;
-  while (lastHead && lastHead.next) {
-    size++;
+
+  let length = 0;
+  let currentHead = head;
+  while (currentHead) {
+    length++;
+    currentHead = currentHead.next;
+  }
+
+  let index = length - (k % length) - 1;
+
+  currentHead = head;
+  while (index) {
+    currentHead = currentHead.next;
+    index--;
+  }
+
+  let lastHead = currentHead;
+  while (lastHead.next) {
     lastHead = lastHead.next;
   }
 
-  size++;
-
-  const index = k % size;
-  if (index === 0) {
-    return head;
-  }
-  const dummyHead = new ListNode(-1);
-  dummyHead.next = head;
-  let x = size - index;
-  let currentHead = dummyHead.next;
-  while (--x) {
-    currentHead = currentHead.next;
-  }
-  lastHead.next = dummyHead.next
-  dummyHead.next = currentHead.next;
+  lastHead.next = head;
+  const ans = currentHead.next;
   currentHead.next = null;
-  return dummyHead.next
+
+  return ans;
 }
