5616a831e4be4075542aae4121c5f2ca0cd6f912
descire <daiqingyunc@gmail.com>
2022-03-16 23:57:57 +0800

    feat(1721): 优化代码

--- Linked-List/1721/solution1.js
+++ Linked-List/1721/solution1.js
@@ -6,37 +6,27 @@ const swapNodes = function(head, k) {
     let length = 0;
 
     let x = null;
-    let xPreHead = null;
 
     let currentHead = head;
     while (currentHead) {
         length++;
-        if (length === k - 1) {
-            xPreHead = currentHead;
-        }
         if (length === k) {
             x = currentHead;
         }
         currentHead = currentHead.next;
     }
 
-    const xSufHead = x.next;
-
     let k2 = length - k;
     let y = null;
-    let yPreHead = null;
     currentHead = head;
     while (k2--) {
-        yPreHead = currentHead;
         currentHead = currentHead.next;
     }
     y = currentHead;
-    const ySufHead = y.next;
 
     const xval = x.val;
-    const yval = y.val;
 
-    x.val = yval;
+    x.val = y.val;
     y.val = xval;
 
     return head;
