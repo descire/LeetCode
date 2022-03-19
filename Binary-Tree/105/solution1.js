f7c44f7aaab4ce0e1c31942114388cb756195749
descire <daiqingyunc@gmail.com>
2022-03-19 19:19:16 +0800

    feat(105): 递归

--- Binary-Tree/105/solution1.js
+++ Binary-Tree/105/solution1.js
@@ -1,25 +1,20 @@
 /**
- * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
- * 
- * 105. 从前序与中序遍历序列构造二叉树
- * 
- * Medium
- * 
- * 根 左 右
- * 
- * 左 根 右
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const buildTree = (preorder, inorder) => {
   if (preorder.length === 0) {
-    return null
+    return null;
   }
-  const rootValue = preorder.shift()
-  const root = new TreeNode(rootValue)
-  // 左右子树的分割点
-  const index = inorder.indexOf(rootValue)
 
-  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index))
-  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1))
+  const rootValue = preorder.shift();
 
-  return root
+  const root = new TreeNode(rootValue);
+
+  const index = inorder.indexOf(rootValue);
+
+  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
+  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1));
+
+  return root;
 }
