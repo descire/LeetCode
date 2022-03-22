5016dfd244c7a26f9fbc79e8323de9fed0f45c34
descire <daiqingyunc@gmail.com>
2022-03-22 23:43:54 +0800

    feat(1008): 更新注释

--- Binary-Tree/1008/solution1.js
+++ Binary-Tree/1008/solution1.js
@@ -1,14 +1,6 @@
 /**
- * https://leetcode-cn.com/problems/construct-binary-search-tree-from-preorder-traversal/
- * 
- * 1008. 先序遍历构造二叉树
- * 
- * Medium
- * 
- * 根 左 右
- * 
- * 76ms 90.91%
- * 34.7mb 42.42%
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(n)
  */
 const bstFromPreorder = preorder => {
   // 终止条件
