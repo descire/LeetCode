b4f7592617ee05e6c2c576844119d0dade56a165
descire <daiqingyunc@gmail.com>
2022-05-02 12:53:34 +0800

    feat(1721): 优化解法

--- Linked-List/1721/solution2.js
+++ Linked-List/1721/solution2.js
@@ -3,25 +3,25 @@
  * 空间复杂度：O(1)
  */
 const swapNodes = function(head, k) {
-    let currentHead = head;
+    let currentHead1 = head;
     let currentHead2 = head;
-    let x;
-    while (currentHead) {
-        if (k === 0) {
-            currentHead2 = currentHead2.next;
-        }
-        if (k > 0) {
-            if (k === 1) {
-                x = currentHead;
-            }
-            k--;
-        }
-        currentHead = currentHead.next;
+
+    while (--k) {
+        currentHead1 = currentHead1.next;
     }
 
+    const x = currentHead1;
+
+    while (currentHead1.next) {
+        currentHead1 = currentHead1.next;
+        currentHead2 = currentHead2.next;
+    }
+
+    const y = currentHead2
+
     const temp = x.val;
-    x.val = currentHead2.val;
-    currentHead2.val = temp;
+    x.val = y.val;
+    y.val = temp;
 
     return head;
 };
