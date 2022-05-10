f303aee51ff49b6acabb737ab03f5c87ef3a38d5
descire <daiqingyunc@gmail.com>
2022-05-11 00:42:54 +0800

    feat(105): 第一种解法

---
+++ Binary-Tree/1008/solution2.js
@@ -0,0 +1,17 @@
+
+const buildTree = (preorder, inorder) => {
+    if (preorder.length === 0) {
+        return null;
+    }
+
+    const rootValue = preorder.shift();
+
+    const root = new TreeNode(rootValue);
+
+    const index = inorder.indexOf(rootValue);
+
+    root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
+    root.right = buildTree(preorder.slice(index), inorder.slice(index + 1));
+
+    return root;
+}
--- Binary-Tree/105/solution1.js
+++ Binary-Tree/105/solution1.js
@@ -1,20 +1,22 @@
 /**
- * 时间复杂度：O(n)
+ * 时间复杂度：O(n^2)
  * 空间复杂度：O(n)
  */
 const buildTree = (preorder, inorder) => {
+  // 递归中止条件
   if (preorder.length === 0) {
     return null;
   }
 
-  const rootValue = preorder.shift();
+  const rootElement = preorder[0];
 
-  const root = new TreeNode(rootValue);
+  const root = new TreeNode(rootElement);
 
-  const index = inorder.indexOf(rootValue);
+  const rootElementIndex = inorder.indexOf(rootElement);
 
-  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
-  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1));
+  root.left = buildTree(preorder.slice(1, rootElementIndex + 1), inorder.slice(0, rootElementIndex));
+
+  root.right = buildTree(preorder.slice(rootElementIndex + 1), inorder.slice(rootElementIndex + 1));
 
   return root;
 }
