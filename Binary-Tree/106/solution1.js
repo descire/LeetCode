f59b33fdfa0ef6e1dad7298bf33405a906bc6e15
descire <daiqingyunc@gmail.com>
2022-05-14 20:17:49 +0800

    feat(106): 递归解法

--- Binary-Tree/106/solution1.js
+++ Binary-Tree/106/solution1.js
@@ -1,17 +1,19 @@
 /**
- * 时间复杂度：O(n)
+ * 时间复杂度：O(n^2)
  * 空间复杂度：O(n)
  * 
  */
 const buildTree = (inorder, postorder) => {
   if (inorder.length === 0) {
-    return null
+    return null;
   }
 
-  const rootValue = postorder.pop()
-  const root = new TreeNode(rootValue)
-  const index = inorder.indexOf(rootValue)
-  root.left = buildTree(inorder.slice(0, index), postorder.slice(0, index))
-  root.right = buildTree(inorder.slice(index + 1), postorder.slice(index))
-  return root
+  const el = postorder.pop();
+  const root = new TreeNode(el);
+  const index = inorder.indexOf(el);
+
+  root.left = buildTree(inorder.slice(0, index), postorder.slice(0, index));
+  root.right = buildTree(inorder.slice(index + 1), postorder.slice(index));
+
+  return root;
 }
