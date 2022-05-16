a30088e7c30619e7a955983656d3978def2977f6
descire <daiqingyunc@gmail.com>
2022-05-16 23:44:55 +0800

    feat(102): 更新迭代解法

--- Binary-Tree/102/solution1.js
+++ Binary-Tree/102/solution1.js
@@ -1,35 +1,28 @@
 /**
- * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
- * 
- * 102. 二叉树的层次遍历
- * 
- * Medium
- * 
- * BFS
- * 
- * 88ms 67.65%
- * 34.6mb 58.21%
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
 const levelOrder = root => {
-  const ans = []
   if (!root) {
-    return ans
+    return [];
   }
 
-  const queue = [root]
+  const ans = [];
+  let queue = [root];
 
   while (queue.length) {
-    const children = []
-    const max = queue.length
+    const len = queue.length
+    const item = [];
+    for (let i = 0; i < len; i++) {
+      const currentRoot = queue.shift();
+      item.push(currentRoot.val);
 
-    for (let i = 0; i < max; i++) {
-      const item = queue.pop()
-      children.push(item.val)
-      item.left && queue.unshift(item.left)
-      item.right && queue.unshift(item.right)
+      currentRoot.left && queue.push(currentRoot.left);
+      currentRoot.right && queue.push(currentRoot.right);
     }
-    ans.push(children)
+
+    ans.push(item);
   }
 
-  return ans
+  return ans;
 }
