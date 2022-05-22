10e0cfb5432634e9b15cb1656d24ee06e02a22e9
descire <daiqingyunc@gmail.com>
2022-05-23 01:56:51 +0800

    feat(437): 深度优先搜索

--- Binary-Tree/437/solution1.js
+++ Binary-Tree/437/solution1.js
@@ -1,33 +1,27 @@
 /**
- * https://leetcode-cn.com/problems/path-sum-iii/
- * 
- * 437. 路径总和 III
- * 
- * Easy
- * 
- * 128ms 74.51%
- * 37.1mb 15.63%
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(n)
  */
 const pathSum = (root, sum) => {
   if (!root) {
-    return 0
+    return 0;
   }
 
-  return countPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum)
+  return rootSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+}
 
-  function countPath (root, sum) {
-    let ans = 0
-    if (!root) {
-      return ans
-    }
-
-    if (root.val === sum) {
-      ans++
-    }
-
-    ans += countPath(root.left, sum - root.val)
-    ans += countPath(root.right, sum - root.val)
-
-    return ans
+function rootSum (root, sum) {
+  if (!root) {
+    return 0;
   }
+
+  let ans = 0;
+  if (root.val === sum) {
+    ans++;
+  }
+
+  ans += rootSum(root.left, sum - root.val);
+  ans += rootSum(root.right, sum - root.val);
+
+  return ans;
 }
