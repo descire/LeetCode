c976c2fa0ef602d8a7234dcf4d196af297be9251
descire <daiqingyunc@gmail.com>
2022-05-25 23:59:01 +0800

    feat(450): 递归+BST

--- Binary-Tree/450/solution1.js
+++ Binary-Tree/450/solution1.js
@@ -1,38 +1,29 @@
 /**
- * https://leetcode-cn.com/problems/delete-node-in-a-bst/
- * 
- * 450. 删除二叉搜索树中的节点
- * 
- * Medium
- * 
- * 116ms 100%
- * 42.3mb 30.00%
- * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
-const deleteNode = (root, key) => {
+ const deleteNode = (root, key) => {
   if (!root) {
-    return null
+    return null;
   }
 
-  const rootValue = root.val
-
-  if (rootValue > key) {
-    root.left = deleteNode(root.left, key)
-  } else if (rootValue < key) {
-    root.right = deleteNode(root.right, key)
+  const rootVal = root.val;
+  if (rootVal > key) {
+    root.left = deleteNode(root.left, key);
+  } else if (rootVal < key) {
+    root.right = deleteNode(root.right, key);
   } else {
     if (root.left && root.right) {
-      // 找一个最小的替换当前根节点，再删除这个替换的节点。
-      let min = root.right
+      let min = root.right;
       while (min.left) {
-        min = min.left
+        min = min.left;
       }
-      root.val = min.val
-      root.right = deleteNode(root.right, min.val)
+      root.val = min.val;
+      root.right = deleteNode(root.right, min.val);
     } else {
-      return root.left ? root.left : root.right
+      return root.left ? root.left : root.right;
     }
   }
 
-  return root
+  return root;
 }
